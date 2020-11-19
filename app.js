be1eba3d9a8710cc54c1630e98d999fa2c138f8b
WANZILA RONNY <wanzilro@gmail.com>
2020-11-19 16:40:15 +0300

    feat: adding the studentDetails, search, studentLisview

--- app.js
+++ app.js
@@ -8,36 +8,49 @@ const morgan = require('morgan');
 const app = express();
 const port = process.env.PORT || 3000;
 
-const homerouter = express.Router();
+const homeRouter = express.Router();
+const registryRouter = express.Router();
+
+const nav = [
+  { link: '/home', title: 'Home' },
+  { link: '/admin', title: 'Admin' },
+  { link: '/registry', title: 'Registry' },
+  { link: '/structure', title: 'Structure' },
+  { link: '/lecturer', title: 'Lecturers' },
+  { link: '/student', title: 'Student' },
+  { link: '/communications', title: 'Communication' },
+  { link: '/payment', title: 'Payment' },
+  { link: '/library', title: 'Library' },
+  { link: '/report', title: 'Report' }
+];
+const title = { title: 'Student Records System'};
+const studentRouter = require('./src/routes/studentRoutes')(nav, title);
 
 app.use(morgan('tiny'));
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
-homerouter.route('/home')
+homeRouter.route('/home')
   .get((req, res) => {
     res.send('hellow people');
   });
 
-app.use('/', homerouter);
+registryRouter.route('/registry')
+  .get((req, res) => {
+    res.send('reg hellow');
+  });
+
+app.use('/', homeRouter);
+app.use('/', registryRouter);
+app.use('/student', studentRouter);
+
 app.get('/', (req, res) => {
   // res.send('hellow');
   // res.sendFile(path.join(__dirname, 'views/index.html'));
   res.render(
     'index',
     {
-      nav: [
-        { link: '/home', title: 'Home' },
-        { link: '/admin', title: 'Admin' },
-        { link: '/registry', title: 'Registry' }, 
-        { link: '/structure', title: 'Structure' },
-        { link: '/lecturer', title: 'Lecturers' },
-        { link: '/student', title: 'Student' }, 
-        { link: '/communications', title: 'Communication' },
-        { link: '/payment', title: 'Payment' },
-        { link: '/library', title: 'Library' },
-        { link: '/report', title: 'Report' }
-      ],
+      nav,
       title: 'Student Records System'
     }
   );
